6c5f968bb226b0a058d48d9d4e2e7aec35e0e3fe
prajwal-934 <patilprajwal982@gmail.com>
2022-08-31 14:46:35 +0530

    stateless

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -6,46 +6,50 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Material(
         color: Colors.white,
-        child: Column(
-          children: [
-            Image.asset("assets/images/login.png"),
-            SizedBox(
-              height: 20,
-            ),
-            const Text(
-              "Welcome",
-              style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  fontStyle: FontStyle.italic,
-                  color: Colors.purple),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 15, vertical: 6),
-              child: Column(
-                children: [
-                  TextFormField(
-                    decoration: InputDecoration(labelText: "Enter Username"),
-                  ),
-                  TextFormField(
-                    obscureText: true,
-                    decoration: InputDecoration(labelText: "Enter Password"),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  ElevatedButton(
-                      style:
-                          TextButton.styleFrom(backgroundColor: Colors.purple),
-                      onPressed: () => {print("Button Clicked")},
-                      child: Text("Login"))
-                ],
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Image.asset("assets/images/login.png"),
+              SizedBox(
+                height: 20,
               ),
-            )
-          ],
+              const Text(
+                "Welcome",
+                style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    fontStyle: FontStyle.italic,
+                    color: Colors.purple),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 15, vertical: 6),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: InputDecoration(labelText: "Enter Username"),
+                    ),
+                    TextFormField(
+                      obscureText: true,
+                      decoration: InputDecoration(labelText: "Enter Password"),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    ElevatedButton(
+                        style: TextButton.styleFrom(
+                            backgroundColor: Colors.purple),
+                        onPressed: () {
+                          print("buttonclicked");
+                        },
+                        child: Text("Login"))
+                  ],
+                ),
+              )
+            ],
+          ),
         ));
   }
 }
